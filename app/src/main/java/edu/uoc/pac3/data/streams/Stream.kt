69667669bc1bedc6e7e6fc6bba58b0b9e6e0a4e8
mbustamante2020 <mbustama@uoc.edu>
2020-11-16 19:35:14 +0100

    Serialización de Stream

--- app/src/main/java/edu/uoc/pac3/data/streams/Stream.kt
+++ app/src/main/java/edu/uoc/pac3/data/streams/Stream.kt
@@ -1,15 +1,20 @@
 package edu.uoc.pac3.data.streams
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 
 /**
  * Created by alex on 07/09/2020.
  */
-
+@Serializable
 data class Stream(
-    val userName: String? = null,
-    val title: String? = null,
+        @SerialName("user_name")  val userName: String? = null,
+        @SerialName("title")  val title: String? = null,
+        @SerialName("thumbnail_url")  val thumbnail_url: String? = null,
 )
 
+@Serializable
 data class StreamsResponse(
     val data: List<Stream>? = null,
 )
