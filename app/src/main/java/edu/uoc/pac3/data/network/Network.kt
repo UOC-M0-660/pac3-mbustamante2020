b1dc0ed9a929c56dd9bf9711a493fd099364581e
mbustamante2020 <mbustama@uoc.edu>
2020-11-16 19:32:17 +0100

    Implementación de HttpClient

--- app/src/main/java/edu/uoc/pac3/data/network/Network.kt
+++ app/src/main/java/edu/uoc/pac3/data/network/Network.kt
@@ -1,8 +1,21 @@
 package edu.uoc.pac3.data.network
 
 import android.content.Context
+import android.util.Log
+import com.moczul.ok2curl.CurlInterceptor
+import com.moczul.ok2curl.logger.Loggable
+import edu.uoc.pac3.data.SessionManager
+import edu.uoc.pac3.data.oauth.OAuthConstants
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.serialization.json.Json
+
 
 /**
  * Created by alex on 07/09/2020.
@@ -14,6 +27,44 @@ object Network {
     fun createHttpClient(context: Context): HttpClient {
         return HttpClient(OkHttp) {
             // TODO: Setup HttpClient
+            // Json
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(json)
+            }
+            // Logging
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Ktor", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            // Timeout
+            install(HttpTimeout) {
+                requestTimeoutMillis = 15000L
+                connectTimeoutMillis = 15000L
+                socketTimeoutMillis = 15000L
+            }
+            // Apply to All Requests
+            defaultRequest {
+                //Log.d("OAuth", "Access Token: ${SessionManager(context).getRefreshToken()} ")
+                //parameter("authorization", SessionManager(context).getAccessToken())
+                // Content Type
+                if (this.method != HttpMethod.Get) contentType(ContentType.Application.Json)
+                accept(ContentType.Application.Json)
+            }
+            // Optional OkHttp Interceptors
+            engine {
+                addInterceptor(CurlInterceptor(Loggable { Log.v("Curl", it) }))
+            }
         }
+    }
+
+    //2 import
+    private val json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+        encodeDefaults = false
     }
 }
