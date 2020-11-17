f78c5de161f4fd9be664a1e80df94f28f25e9804
mbustamante2020 <mbustama@uoc.edu>
2020-11-18 00:30:37 +0100

    Agregados todos los endpoints

--- app/src/main/java/edu/uoc/pac3/data/network/Endpoints.kt
+++ app/src/main/java/edu/uoc/pac3/data/network/Endpoints.kt
@@ -8,8 +8,12 @@ object Endpoints {
     // OAuth2 API Endpoints
     private const val oauthBaseUrl = "https://id.twitch.tv/oauth2"
     // TODO: Add all remaining endpoints
+    const val oauthAuthorize = "$oauthBaseUrl/authorize"
+    const val oauthToken = "$oauthBaseUrl/token"
 
     // Twitch API Endpoints
     private const val twitchBaseUrl = "https://api.twitch.tv/helix"
     // TODO: Add all remaining endpoints
+    const val oauthStreams = "$twitchBaseUrl/streams"
+    const val oauthUser = "$twitchBaseUrl/users"
 }
