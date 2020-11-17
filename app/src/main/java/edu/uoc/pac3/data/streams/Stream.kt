0bbc6a2d1ba3c3ff9fa427fde6602a245da0f7c2
mbustamante2020 <mbustama@uoc.edu>
2020-11-18 00:36:04 +0100

    Se agregó thumbnail_url

--- app/src/main/java/edu/uoc/pac3/data/streams/Stream.kt
+++ app/src/main/java/edu/uoc/pac3/data/streams/Stream.kt
@@ -11,7 +11,7 @@ import kotlinx.serialization.Serializable
 data class Stream(
         @SerialName("user_name")  val userName: String? = null,
         @SerialName("title")  val title: String? = null,
-        @SerialName("thumbnail_url")  val thumbnail_url: String? = null,
+        @SerialName("thumbnail_url")  val thumbnailUrl: String? = null,
 )
 
 @Serializable
