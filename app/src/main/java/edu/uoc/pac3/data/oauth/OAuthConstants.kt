b482c8265791a6a86b8ccbcd01c92bb933a998e1
mbustamante2020 <mbustama@uoc.edu>
2020-11-19 01:11:21 +0100

    constantes

--- app/src/main/java/edu/uoc/pac3/data/oauth/OAuthConstants.kt
+++ app/src/main/java/edu/uoc/pac3/data/oauth/OAuthConstants.kt
@@ -1,10 +1,15 @@
 package edu.uoc.pac3.data.oauth
 
+import java.util.*
 /**
  * Created by alex on 07/09/2020.
  */
 object OAuthConstants {
 
     // TODO: Set OAuth2 Variables
-
+    val uniqueState = UUID.randomUUID().toString()
+    const val clientID = "93e9vxk1j9jauy58qlez0oqrrmlfa3"
+    const val clientSecret = "tedrddnqnb4h3sqfv1f2ibybgn870l"
+    const val redirectUri = "https://www.twitch.tv/"
+    val scopes = listOf<String>("user:read:email", "user:edit")
 }
