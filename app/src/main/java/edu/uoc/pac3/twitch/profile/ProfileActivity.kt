d9963d5a20f273009df6e9ffdce7151fc1294519
mbustamante2020 <mbustama@uoc.edu>
2020-12-04 23:21:49 +0100

    Modificación en Multithreading

--- app/src/main/java/edu/uoc/pac3/twitch/profile/ProfileActivity.kt
+++ app/src/main/java/edu/uoc/pac3/twitch/profile/ProfileActivity.kt
@@ -1,15 +1,17 @@
 package edu.uoc.pac3.twitch.profile
 
-import android.app.ActivityManager
-import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.webkit.CookieManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import edu.uoc.pac3.LaunchActivity
@@ -18,13 +20,7 @@ import edu.uoc.pac3.data.SessionManager
 import edu.uoc.pac3.data.TwitchApiService
 import edu.uoc.pac3.data.network.Network
 import edu.uoc.pac3.data.user.User
-import edu.uoc.pac3.oauth.LoginActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.w3c.dom.Text
-import java.lang.Exception
+import kotlinx.coroutines.*
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -49,28 +45,34 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun getUser() {
-        val user = runBlocking {
-            TwitchApiService(Network.createHttpClient(this@ProfileActivity)).getUser()
-        }
-        if( user == null ) {
-            Log.i("OAuth ProfileActivity", "getUser")
-            startActivity(Intent(this, LoginActivity::class.java))
-        } else {
-            loadUser(user)
+        lifecycleScope.launch {
+            whenStarted {
+                val user = withContext(Dispatchers.IO) {
+                    TwitchApiService(Network.createHttpClient(this@ProfileActivity)).getUser()
+                }
+                if (user == null) {
+                    Toast.makeText(applicationContext, R.string.error_profile, Toast.LENGTH_LONG).show()
+                } else {
+                    loadUser(user)
+                }
+            }
         }
     }
 
     private fun updateUser() {
-        //val network = Network.createHttpClient(this)  GlobalScope.launch(Dispatchers.Main)
-        val description = findViewById<TextView>(R.id.userDescriptionEditText).text.toString()
-        val user = runBlocking {
-            TwitchApiService(Network.createHttpClient(this@ProfileActivity)).updateUserDescription(description)
-        }
-        if( user == null ) {
-            Log.i("OAuth ProfileActivity", "updateUser ")
-            startActivity(Intent(this, LoginActivity::class.java))
-        } else {
-            loadUser(user)
+        lifecycleScope.launch {
+            whenStarted {
+                val description = findViewById<TextView>(R.id.userDescriptionEditText).text.toString()
+                val user = withContext(Dispatchers.IO) {
+                    TwitchApiService(Network.createHttpClient(this@ProfileActivity)).updateUserDescription(description)
+                }
+
+                if (user == null) {
+                    Toast.makeText(applicationContext, R.string.error_profile_update, Toast.LENGTH_LONG).show()
+                } else {
+                    loadUser(user)
+                }
+            }
         }
     }
 
@@ -81,23 +83,29 @@ class ProfileActivity : AppCompatActivity() {
 
         var image = user?.userImageUrl.toString()
         image = Regex("\\{height\\}|\\{width\\}").replace(image, "200")
-
-        Glide.with(this@ProfileActivity)  //2
-                .load(image)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(findViewById<ImageView>(R.id.imageView))
+        AsyncTask.execute {
+            runOnUiThread {
+                Glide.with(this@ProfileActivity)  //2
+                        .load(image)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .into(findViewById<ImageView>(R.id.imageView))
+            }
+        }
     }
 
     private fun logout() {
-        try {
-            SessionManager(this).clearAccessToken()
-            SessionManager(this).clearRefreshToken()
-            (this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).clearApplicationUserData()
-        } catch (e: Exception) {
-            Log.d("OAuth Exception", "Access Token: ${e.toString()}")
+        lifecycleScope.launch {
+            async {
+                CookieManager.getInstance().removeAllCookies(null)
+                CookieManager.getInstance().flush()
+            }
+            async {
+                SessionManager(this@ProfileActivity).clearAccessToken()
+                SessionManager(this@ProfileActivity).clearRefreshToken()
+            }
         }
-        Log.d("OAuth logout", "Access Token: ${SessionManager(this).getAccessToken()}, Refresh Token: ${SessionManager(this).getRefreshToken()}")
-
-        startActivity(Intent(this, LaunchActivity::class.java))
+        val intent = Intent(this, LaunchActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
